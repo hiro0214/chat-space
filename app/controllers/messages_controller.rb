da81c1e37ec6deb487af5e05d9306e85e8926115
hiro0214 <leonhurt2@gmail.com>
2019-09-14 14:14:58 +0900

    commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,10 +9,15 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    @message.save
+    if @message.save
       respond_to do |format|
         format.json
       end
+      else
+       @messages = @group.messages.includes(:user)
+	     flash.now[:alert] = 'メッセージを入力してください。'
+	     render :index
+    end
  end
 
   private

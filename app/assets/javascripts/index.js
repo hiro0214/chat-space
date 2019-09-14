fca6d2f110bb32734c88b9ce9eaa89dbfcabe754
hiro0214 <leonhurt2@gmail.com>
2019-09-14 16:38:31 +0900

    commit

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -16,7 +16,7 @@ $(function(){
     return html;
   }
 
-  $("form").submit(function(e){
+  $("#new_message").on("submit",function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr("action")

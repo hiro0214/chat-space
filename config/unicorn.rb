5014bde4f8298f60fb738e21aa750b340d89f769
hiro0214 <leonhurt2@gmail.com>
2019-09-17 14:55:43 +0900

    c

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,10 +7,10 @@ worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
 working_directory "#{app_path}/current"
-pid "#{app_path}/tmp/pids/unicorn.pid"
-listen "#{app_path}/tmp/sockets/unicorn.sock"
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{shared_path}/tmp/pids/unicorn.pid"

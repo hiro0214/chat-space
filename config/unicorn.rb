15dd706caff3b1e456acdf1d4f8c2866c6e022a7
hiro0214 <leonhurt2@gmail.com>
2019-09-17 13:38:47 +0900

    c

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,6 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
 app_path = File.expand_path('../../../', __FILE__)
+shared_path = File.expand_path('../../../shared/', __FILE__)
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
@@ -12,7 +13,7 @@ stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/tmp/pids/unicorn.pid"
+pid "#{shared_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
 listen "#{app_path}/tmp/sockets/unicorn.sock"
